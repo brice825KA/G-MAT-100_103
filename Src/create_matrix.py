298f99d3cfc80964e5942fc5750fbc61d14d4233
brice825KA <brice825KA@users.noreply.github.com>
2026-07-26 17:54:15 +0100

    main Remove file matrix_identitie.py

--- Src/create_matrix.py
+++ Src/create_matrix.py
@@ -1,5 +1,26 @@
 import math
 
+def gen_mat(size):
+    new_mat = []
+
+    for k in range(size):
+        ligne = [0.0] * size
+        new_mat.append(ligne)
+    return new_mat
+
+def matrix_identitie(mat_init):
+    size = int(math.sqrt(len(mat_init)))
+    new_mat = gen_mat(size)
+    
+    for i in range(len(new_mat)):
+        for j in range(len(new_mat[i])):
+            if i == j:
+                new_mat[i][j] = 1.0
+            else:
+                new_mat[i][j] = 0.0
+    return new_mat
+
+
 def find_min_square(n: int) -> int:
     square = n
     while 1:
